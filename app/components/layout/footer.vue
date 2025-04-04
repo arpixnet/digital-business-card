82c2c8f04169f93b7ec110331f17bc477891541c
Leonardo Vizcaya Savchenko <lvizcaya@arpixnet.com>
2025-04-03 21:53:50 -0500

    Update footer.vue

--- app/components/layout/footer.vue
+++ app/components/layout/footer.vue
@@ -1,10 +1,6 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
     <div class="w-full px-4 md:px-0 py-[70px] flex flex-col md:flex-row justify-center items-center gap-0 md:gap-2 bg-slate-950">
-        <div class="w-fit text-xs">{{ new Date().getFullYear() }} © Leonardo Vizcaya - Todos los derechos reservados</div> <span class="hidden md:block">|</span> 
+        <div class="w-fit text-xs text-center">{{ new Date().getFullYear() }} © Leonardo Vizcaya - Todos los derechos reservados</div> <span class="hidden md:block">|</span> 
         <ArpixLogo />
     </div>
 </template>
